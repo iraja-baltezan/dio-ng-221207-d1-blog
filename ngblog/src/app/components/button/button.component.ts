bbdfc5c1bcd7688159b4c94aed72252067b1baaf
iraja-baltezan <iraja.baltezan@gmail.com>
2023-01-05 18:47:20 -0300

    factor: criado componente botão

---
+++ ngblog/src/app/components/button/button.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+type TRouterLinkParam = string | number | object;
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent {
+  @Input()
+  routerLink?: string | TRouterLinkParam[] | null;
+
+  @Input()
+  iconSrc?: string;
+
+
+}
